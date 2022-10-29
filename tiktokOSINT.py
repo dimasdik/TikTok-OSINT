c866c8675b2dbfcec1b87f5935a2eb59a84ff243
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-10-30 01:26:23 +0200

    just some code formatting

--- tiktokOSINT.py
+++ tiktokOSINT.py
@@ -41,25 +41,26 @@ class TikTokOSINT:
 		:params: none
 		:return:none
 		"""
-		r = requests.get(f'http://tiktok.com/{self.username}', headers={'User-Agent':random.choice(user_agents)})
+		r = requests.get(f'http://tiktok.com/{self.username}', headers={'User-Agent': random.choice(user_agents)})
 		soup = BeautifulSoup(r.text, "html.parser")
-		content = soup.find_all("script", attrs={"type":"application/json", "crossorigin":"anonymous"})
+		content = soup.find_all("script", attrs={"type": "application/json", "crossorigin": "anonymous"})
 		content = json.loads(content[0].contents[0])
 		profile_data = {"UserID":content["props"]["pageProps"]["userInfo"]["user"]["id"],
-			"username":content["props"]["pageProps"]["userInfo"]["user"]["uniqueId"],
-			"nickName":content["props"]["pageProps"]["userInfo"]["user"]["nickname"],
-			"bio":content["props"]["pageProps"]["userInfo"]["user"]["signature"],
-			"profileImage":content["props"]["pageProps"]["userInfo"]["user"]["avatarLarger"],
-			"following":content["props"]["pageProps"]["userInfo"]["stats"]["followingCount"],
-			"followers":content["props"]["pageProps"]["userInfo"]["stats"]["followerCount"],
-			"likes":content["props"]["pageProps"]["userInfo"]["stats"]["heart"],
-			"videos":content["props"]["pageProps"]["userInfo"]["stats"]["videoCount"],
-			"verified":content["props"]["pageProps"]["userInfo"]["user"]["verified"]}
+			"username": content["props"]["pageProps"]["userInfo"]["user"]["uniqueId"],
+			"nickName": content["props"]["pageProps"]["userInfo"]["user"]["nickname"],
+			"bio": content["props"]["pageProps"]["userInfo"]["user"]["signature"],
+			"profileImage": content["props"]["pageProps"]["userInfo"]["user"]["avatarLarger"],
+			"following": content["props"]["pageProps"]["userInfo"]["stats"]["followingCount"],
+			"followers": content["props"]["pageProps"]["userInfo"]["stats"]["followerCount"],
+			"likes": content["props"]["pageProps"]["userInfo"]["stats"]["heart"],
+			"videos": content["props"]["pageProps"]["userInfo"]["stats"]["videoCount"],
+			"verified": content["props"]["pageProps"]["userInfo"]["user"]["verified"]}
 
 		return profile_data
 
 	def download_profile_picture(self):
-		"""Downloads the profile picture
+		"""
+		Downloads the profile picture
 		:params: none
 		:return: none
 		"""
@@ -97,7 +98,8 @@ class TikTokOSINT:
 				i += 1
 
 	def print_data(self):
-		"""Prints out the data to the cmd line
+		"""
+		Prints out the data to the cmd line
 		:params:none
 		:return: none
 		"""
